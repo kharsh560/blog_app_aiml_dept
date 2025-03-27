3877d6a062f8a52c048960d552972362415ed6c3
Kumar Harsh <kharsh560@gmail.com>
2025-03-28 01:28:29 +0530

    Used simpler version of CORS, Let's see!

--- blog_app_backend/src/app.js
+++ blog_app_backend/src/app.js
@@ -5,30 +5,30 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 // CORS setup
-// app.use(
-//   cors({
-//     origin: process.env.CORS_ORIGIN,
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
-const whitelist = ["https://mait-aiml-official-blogspace.netlify.app"];
+// const whitelist = ["https://mait-aiml-official-blogspace.netlify.app"];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like server-to-server requests) or those from the whitelist
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not Allowed by CORS"));
-    }
-  },
-  methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     // Allow requests with no origin (like server-to-server requests) or those from the whitelist
+//     if (!origin || whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not Allowed by CORS"));
+//     }
+//   },
+//   methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
+//   credentials: true,
+// };
 
-// Middlewares -->
-app.use(cors(corsOptions));
+// // Middlewares -->
+// app.use(cors(corsOptions));
 
 // For data from URL forms/body, when we accept data, we do some restrictions!
 app.use(express.json({ limit: "16kb" }));
